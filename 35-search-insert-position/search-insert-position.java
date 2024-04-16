64e9322ddaa172a36d7a547a530eecbb9105fbb4
YossefYasser <48572029+YossefYasser@users.noreply.github.com>
2024-04-16 02:48:32 +0200

    Time: 0 ms (100.00%) | Memory: 43.1 MB (16.38%) - LeetSync

---
+++ 35-search-insert-position/search-insert-position.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int searchInsert(int[] arr, int key) {
+         int low =0; int high = arr.length-1; int mid =0;
+        while(low<=high)
+        {
+            mid = (low+high)/2;
+            if(arr[mid ] == key) return mid;
+            if(arr[mid]>key)
+                high = mid-1;
+            else
+                low = mid +1;
+
+        }
+        return low;
+    }
+}
