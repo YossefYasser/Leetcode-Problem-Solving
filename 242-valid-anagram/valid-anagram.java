38b2f5c21b24b7785630087529b0c208f9cf33cf
YossefYasser <48572029+YossefYasser@users.noreply.github.com>
2024-05-01 04:28:03 +0300

    Time: 32 ms (5.03%) | Memory: 44.8 MB (17.21%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,66 +1,34 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
 
+        if(s.length() != t.length())
+            return false;
+        HashMap<Character , Integer > map1 = new HashMap<Character , Integer>();
+        HashMap<Character , Integer > map2 = new HashMap<Character , Integer>();
+            int count ;
+        for ( int i =0 ; i<s.length() ;i++)
+        {
+            count =0;
+            if(map1.containsKey(s.charAt(i)))
+                count =map1.get(s.charAt(i));
+            map1.put(s.charAt(i),count+1);
 
-        char[] arr1 = s.toCharArray();
-        char[] arr2 = t.toCharArray();
+            count =0;
+            if(map2.containsKey(t.charAt(i)))
+                count =map2.get(t.charAt(i));
+            map2.put(t.charAt(i),count+1);
+        }
+         for ( char i : map1.keySet())
+         {  if(!map2.containsKey(i))
+                return(false);
+            if(map1.get(i).intValue() !=map2.get(i).intValue() )
+            {   
+                 
 
-        Arrays.sort(arr1);
-        Arrays.sort(arr2);
-        s= Arrays.toString(arr1);
-        t= Arrays.toString(arr2);
-        s= s.toLowerCase();
-        t= t.toLowerCase();
-        
-
-        return s.equals(t);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        // if(s.length() != t.length())
-        //     return false;
-        // HashMap<Character , Integer > map1 = new HashMap<Character , Integer>();
-        // HashMap<Character , Integer > map2 = new HashMap<Character , Integer>();
-        //     int count ;
-        // for ( int i =0 ; i<s.length() ;i++)
-        // {
-        //     count =0;
-        //     if(map1.containsKey(s.charAt(i)))
-        //         count =map1.get(s.charAt(i));
-        //     map1.put(s.charAt(i),count+1);
-
-        //     count =0;
-        //     if(map2.containsKey(t.charAt(i)))
-        //         count =map2.get(t.charAt(i));
-        //     map2.put(t.charAt(i),count+1);
-        // }
-        //  for ( char i : map1.keySet())
-        //  {  if(!map2.containsKey(i))
-        //         return(false);
-        //     if(map1.get(i).intValue() !=map2.get(i).intValue() )
-        //     {   
-        //          System.out.println(map1.get(i).intValue());
-        //          System.out.println(map2.get(i));
-
-        //         return false;
-        //     }
-        //  }
-        //  return true;
+                return false;
+            }
+         }
+         return true;
 
     
     
