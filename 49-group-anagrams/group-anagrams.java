784b39376fb65372c40c360d0ddc1020ceda3edd
YossefYasser <48572029+YossefYasser@users.noreply.github.com>
2024-05-01 06:52:13 +0300

    Time: 7 ms (64.18%) | Memory: 47.6 MB (74.49%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -1,41 +1,22 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-          
-        String[] temp = strs.clone();
-        List<List<String>> res = new ArrayList<>();
+          List<List<String>> res = new ArrayList<>();
         char[] s ;
-                for (int i = 0; i < temp.length; i++) {
-                    s=temp[i].toCharArray();
+        String sorted;
+        HashMap<String,List<String>  > hash = new HashMap<>();
+                for (int i = 0; i < strs.length; i++) {
+                    s=strs[i].toCharArray();
                     Arrays.sort(s);
-                    
-                    temp[i] = new String(s);
+                    sorted = new String(s);
+                    if(!hash.containsKey(sorted))
+                        hash.put(sorted,new ArrayList<>() );
 
+                    hash.get(sorted).add(strs[i]);
                 }
-
-                HashMap<String,List<Integer>  > hash = new HashMap<>();
-
-                for (int i = 0; i < temp.length ; i++) {
-                    if(!hash.containsKey(temp[i]))
-                        hash.put(temp[i],new ArrayList<>() );
-
-                    hash.get(temp[i]).add(i);
-
-                }
-
-               
-
-               for(List<Integer> arr : hash.values())
+               for(List<String> arr : hash.values())
                 {
-                   List<String> anagrams = new ArrayList();
-                    for(int i :arr)
-                    {
-                        anagrams.add(strs[i]);
-
-                    }
-                    res.add(anagrams);
-
+                    res.add(arr);
                 }
-
                 return res;
     } 
 }
