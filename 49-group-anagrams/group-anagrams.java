8475d05f3b069a09483da82bda33cba699cdad31
YossefYasser <48572029+YossefYasser@users.noreply.github.com>
2024-05-01 06:42:29 +0300

    Time: 49 ms (7.06%) | Memory: 49.1 MB (7.41%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.java
@@ -0,0 +1,41 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+          
+        String[] temp = strs.clone();
+        List<List<String>> res = new ArrayList<>();
+        char[] s ;
+                for (int i = 0; i < temp.length; i++) {
+                    s=temp[i].toCharArray();
+                    Arrays.sort(s);
+                    System.out.println(Arrays.toString(s));
+                    temp[i] = new String(s);
+
+                }
+
+                HashMap<String,List<Integer>  > hash = new HashMap<>();
+
+                for (int i = 0; i < temp.length ; i++) {
+                    if(!hash.containsKey(temp[i]))
+                        hash.put(temp[i],new ArrayList<>() );
+
+                    hash.get(temp[i]).add(i);
+
+                }
+
+                System.out.println(hash);
+
+               for(List<Integer> arr : hash.values())
+                {
+                   List<String> anagrams = new ArrayList();
+                    for(int i :arr)
+                    {
+                        anagrams.add(strs[i]);
+
+                    }
+                    res.add(anagrams);
+
+                }
+
+                return res;
+    } 
+}
