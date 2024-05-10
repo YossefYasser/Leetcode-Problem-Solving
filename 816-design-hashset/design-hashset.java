6382c3bc562c900b2379b88be127ee2eb5bb12b5
YossefYasser <48572029+YossefYasser@users.noreply.github.com>
2024-05-11 02:33:39 +0300

    Time: 13 ms (83.18%) | Memory: 54.2 MB (29.00%) - LeetSync

---
+++ 816-design-hashset/design-hashset.java
@@ -0,0 +1,27 @@
+class MyHashSet {
+HashSet<Integer> hash = new HashSet<>();
+    public MyHashSet() {
+        
+    }
+    
+    public void add(int key) {
+        hash.add(key);
+    }
+    
+    public void remove(int key) {
+        if(hash.contains(key))
+            hash.remove(key);
+    }
+    
+    public boolean contains(int key) {
+        return hash.contains(key);
+    }
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet obj = new MyHashSet();
+ * obj.add(key);
+ * obj.remove(key);
+ * boolean param_3 = obj.contains(key);
+ */
