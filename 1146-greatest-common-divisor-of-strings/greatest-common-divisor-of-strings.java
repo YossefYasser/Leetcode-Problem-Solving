6df8129caf36f24eb56c2f85f105ccdc542b92dd
YossefYasser <48572029+YossefYasser@users.noreply.github.com>
2024-05-17 21:57:20 +0300

    Time: 41 ms (5.02%) | Memory: 44.7 MB (9.78%) - LeetSync

---
+++ 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.java
@@ -0,0 +1,51 @@
+class Solution {
+    public String gcdOfStrings(String str1, String str2) {
+       int s1 = str1.length(); int s2 = str2.length();
+       int x =1;
+       int min = Math.min(s1,s2);
+       int max = Math.max(s1,s2);
+       int times;
+       String temp="";
+       String CGD ="";
+       String current;
+       while(x<=min)
+       {
+        if(s1%x ==0 && s2%x ==0)
+        {   
+
+            current= str2.substring(0,x);
+            while(temp.length()!=s1)
+                {temp+=current; }
+            if(!temp.equals(str1))
+                 {
+                    x++;
+                    temp="";
+                    continue;
+                }
+            temp="";
+            while(temp.length()!=s2)
+                 {
+                                          System.out.println(s2);
+
+
+                    temp+=current;
+                }
+            if(!temp.equals(str2))
+                  {          
+                    x++;temp="";continue;
+                    }
+            if(x>CGD.length())
+            {
+                CGD =current;
+                temp="";
+                x++;
+            }
+        }
+        else
+            x++;
+
+       }
+       return CGD;
+
+    }
+}
