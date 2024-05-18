0233a53e111a31b40558700ff1bc09c3c1b3410e
YossefYasser <48572029+YossefYasser@users.noreply.github.com>
2024-05-18 03:07:44 +0300

    Time: 27 ms (5.53%) | Memory: 55.3 MB (54.24%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -1,37 +1,34 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-       
-                int[] pre = new int[nums.length];
-                int[] post = new int[nums.length];
-                for(int i = 0,j=nums.length-1; i < nums.length && j >=0 ; i++,j--)
-                {
-                    if(i==0)
-                    {
-                        pre[i]=nums[i];
-                        post[j]=nums[j];
-                    }
-                    else
-                    {
-                        pre[i] = pre[i-1] * nums[i];
-                        post[j]= post[j+1] * nums[j];
-                    }
-                }
+       int[] pre = new int[nums.length];
+      
+      int[] post = new int[nums.length];
+      int prenum=1;
+      int postnum=1;
+        for(int i=0,j=nums.length-1;i<nums.length && j>=0;j--,i++)
+        {   prenum*= nums[i];
+            postnum*= nums[j];
+            pre[i] =prenum;
+            post[j]= postnum;
+        }
+        System.out.println(Arrays.toString(pre));
+        System.out.println(Arrays.toString(post));
 
-                for (int i = 0; i < nums.length; i++) {
-
-                    if(i==0)
-                    {
-                        nums[i]=post[i+1];
-                    }
-                    else if(i== nums.length-1)
-                    {
-                        nums[i] = pre[i-1];
-                    }
-                    else
-                        nums[i] = pre[i-1]*post[i+1];
-
-                }
-
-                return nums;
+        for(int i=0;i<nums.length;i++)
+        {
+            if(i==0)
+            {
+                nums[i]=post[i+1];
+            }
+            else if ( i == nums.length-1)
+            {
+                nums[i]=pre[i-1];
+            }
+            else
+            {
+                nums[i]=pre[i-1]*post[i+1];
+            }
+        }
+         return nums;      
     }
 }
