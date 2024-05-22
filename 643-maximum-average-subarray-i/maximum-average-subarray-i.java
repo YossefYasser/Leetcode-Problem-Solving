f4c8700ad484f38dc2ae192f508a8b35dfd11331
YossefYasser <48572029+YossefYasser@users.noreply.github.com>
2024-05-22 05:15:06 +0300

    Time: 5 ms (40.12%) | Memory: 56.2 MB (25.93%) - LeetSync

--- 643-maximum-average-subarray-i/maximum-average-subarray-i.java
+++ 643-maximum-average-subarray-i/maximum-average-subarray-i.java
@@ -3,17 +3,15 @@ class Solution {
        int l =0;int r=k-1; double sum =0;
       while(l<=r)
       {sum+= nums[l++];}
-      
       double max=sum;
       l=1;
       r++;
      
       while(r<nums.length)
       {
-         sum = (double)sum-nums[l-1]+nums[r];
+         sum = (double)sum-nums[-1+l++]+nums[r++];
          max=Math.max(sum,max);
-         l++;
-         r++;
+         
       }
       return max/k;
 
