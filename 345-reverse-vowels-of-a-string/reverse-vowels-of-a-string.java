250613d85f2cc5a7a4fad1ba5d9084f6389cc68e
YossefYasser <48572029+YossefYasser@users.noreply.github.com>
2024-05-24 20:40:48 +0300

    Time: 6 ms (33.54%) | Memory: 44.6 MB (83.66%) - LeetSync

--- 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.java
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.java
@@ -1,24 +1,27 @@
 class Solution {
     public String reverseVowels(String s) {
-        StringBuilder x = new StringBuilder(s);
+       char x[]=s.toCharArray();
       HashSet<Character> hash = new HashSet<>(Arrays.asList('a','e','o','u','i','A','E','O','U','I'));
         char temp;      int l =0; int r = s.length()-1;
       while(l<r)
       {
-        if(hash.contains(s.charAt(l)) && hash.contains(s.charAt(r)))
+
+        if(hash.contains(x[l]) && hash.contains(x[r]))
         {
-            temp = s.charAt(l);
-            x.setCharAt(l,s.charAt(r));
-            x.setCharAt(r,temp);
+            
+            temp = x[l];
+
+            x[l]=x[r];
+            x[r]=temp;
             
         }
-       else if(hash.contains(s.charAt(l)) && !hash.contains(s.charAt(r)))
+       else if(hash.contains(x[l]) && !hash.contains(x[r]))
         {
             r--;
             continue;
             
         }
-        else if(!hash.contains(s.charAt(l)) && hash.contains(s.charAt(r)))
+        else if(!hash.contains(x[l]) && hash.contains(x[r]))
         {
             l++;
             continue;
@@ -27,7 +30,7 @@ class Solution {
             l++;r--;
         
       }
-return x.toString();
+return new String(x);
 
     }
 }
