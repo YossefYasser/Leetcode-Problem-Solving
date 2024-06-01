756824942fd07e1cea5695b95b10eeb0a5192817
YossefYasser <48572029+YossefYasser@users.noreply.github.com>
2024-06-01 04:59:32 +0300

    Time: 20 ms (91.34%) | Memory: 54.2 MB (40.96%) - LeetSync

--- 969-number-of-recent-calls/number-of-recent-calls.java
+++ 969-number-of-recent-calls/number-of-recent-calls.java
@@ -1,29 +1,24 @@
 class RecentCounter {
     int counter;
-    ArrayList<Integer> result = new ArrayList<>();
-     ArrayList<Integer> requests = new ArrayList<>();
+    Queue<Integer> q = new LinkedList<>();
 
     public void RecentCounter() {
-        counter =0;
+       
         
     }
     
     public int ping(int t) {
-
-        if ( t>0)
+        
+        q.add(t);
+        while ( q.peek()<t-3000)
         {
-            requests.add(t);
-            this.RecentCounter();
+            q.poll();
            
         }
         
-        for ( int x : requests)
-        {
-            if( x <= t && x >= t-3000)
-            {counter++;}
-        }
+        
 
-        return counter;
+        return q.size();
     }
 }
 
