f7f02f1d1b725c9da3db372b511beb89c6b03609
YossefYasser <48572029+YossefYasser@users.noreply.github.com>
2024-08-03 21:06:58 +0300

    Time: 3 ms (85.96%) | Memory: 44.1 MB (56.26%) - LeetSync

--- 1556-make-two-arrays-equal-by-reversing-subarrays/make-two-arrays-equal-by-reversing-subarrays.java
+++ 1556-make-two-arrays-equal-by-reversing-subarrays/make-two-arrays-equal-by-reversing-subarrays.java
@@ -1,28 +1,8 @@
 class Solution {
     public boolean canBeEqual(int[] target, int[] arr) {
-        HashMap<Integer,Integer> hash1 = new  HashMap<>();
-        HashMap<Integer,Integer> hash2 = new HashMap<>();
+        Arrays.sort(target);
+        Arrays.sort(arr);
 
-        for(int i =0;i<target.length;i++)
-        {
-            if(hash1.containsKey(target[i]))
-                hash1.put(target[i],hash1.get(target[i])+1);
-            else
-                 hash1.put(target[i],1);
-
-            if(hash2.containsKey(arr[i]))
-                hash2.put(arr[i],hash2.get(arr[i])+1);
-            else
-                 hash2.put(arr[i],1);
-        }
-        for(Map.Entry<Integer,Integer> set : hash1.entrySet())
-        {
-            if(hash2.get(set.getKey()) != set.getValue()) 
-            
-            {
-                return false;
-            }
-        }
-    return true;
+       return (Arrays.equals(target,arr));
     }
 }
